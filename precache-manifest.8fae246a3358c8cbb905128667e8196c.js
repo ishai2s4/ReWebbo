7d90b065b4c617bf81c4e27c95f61efe36001728
ishai2s4 <ishai2s4@gmail.com>
2020-10-12 14:47:27 +0530

    Updates

---
+++ precache-manifest.8fae246a3358c8cbb905128667e8196c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "385b03444213fbb03b785dd97a858ab2",
+    "url": "/ReWebbo/index.html"
+  },
+  {
+    "revision": "826804ce5ae97574ee8a",
+    "url": "/ReWebbo/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "3c4be62f6ffb9a79cc07",
+    "url": "/ReWebbo/static/css/main.f8c65234.chunk.css"
+  },
+  {
+    "revision": "826804ce5ae97574ee8a",
+    "url": "/ReWebbo/static/js/2.5a20024f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReWebbo/static/js/2.5a20024f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c4be62f6ffb9a79cc07",
+    "url": "/ReWebbo/static/js/main.475c3e0d.chunk.js"
+  },
+  {
+    "revision": "d09cc3e7c2dee665d4de",
+    "url": "/ReWebbo/static/js/runtime-main.0500b0c5.js"
+  },
+  {
+    "revision": "cd9d6d3a5190d9b9ec669b15c0b88fef",
+    "url": "/ReWebbo/static/media/3.cd9d6d3a.svg"
+  },
+  {
+    "revision": "1586862b676550ac91da492b331f43bf",
+    "url": "/ReWebbo/static/media/avion.1586862b.svg"
+  }
+]);
